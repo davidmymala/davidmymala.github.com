3a570ba348f304a6fe9a2e8e89de5528e3d95be5
David Mymala <dpmymala@ucdavis.edu>
2021-02-02 01:27:20 -0800

    Finished Mad Libs

--- des157/madlibs/script.js
+++ des157/madlibs/script.js
@@ -15,24 +15,27 @@
 
         event.preventDefault();
         
+        //variable for user inputs
         const foode = document.querySelector("#foode").value;
         const food1 = document.querySelector("#food1").value;
         const food2 = document.querySelector("#food2").value;
         const food3 = document.querySelector("#food3").value;
         const food4 = document.querySelector("#food4").value;
 
+        //random array for scripts and user input
 
         const  output  = [`The <strong>${food1}</strong> was excellent as well as the <strong>${food2}</strong>, <strong>${food3},</strong> and <strong>${food4}</strong>. The seasoning was on point! I will be looking forward to come again!`, `The <strong>${food1}</strong> was terrible as well as the <strong>${food2}</strong>, <strong>${food3}</strong>, and <strong>${food4}</strong>. Also, the seasoning was not up to standards. I will not be returning to this establishment.`]; 
 
-        const randomNum = Math.floor(Math.random() * 2)
+        const randomNum = Math.floor(Math.random() * 2);
 
-        const randomOutput = output[randomNum]
+        const randomOutput = output[randomNum];
 
+        //remove content and change color of box
 
         formMain.style.display ="none";
         box.style.background = "#F0DFAA";
         
-        madlibMain.style.fontFamily = "Calibri";
+        madlibMain.style.fontFamily = "Calibri", "Arial";
 
         madlibMain.innerHTML += `
         <a class = "button" href="index.html">&otimes;</a>
